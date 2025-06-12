0bb49ca9783d7d68831573d8c4a5bc9a630100ea
Carson Katri <Carson.katri@gmail.com>
2025-06-12 16:37:48 -0400

    Use package traits for enabling embedded builds

--- Package.swift
+++ Package.swift
@@ -3,31 +3,29 @@
 
 import PackageDescription
 
-let shouldBuildForEmbedded = Context.environment["JAVASCRIPTKIT_EXPERIMENTAL_EMBEDDED_WASM"].flatMap(Bool.init) ?? false
-
 let package = Package(
     name: "ExternalTermFormat",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ExternalTermFormat",
-            targets: ["ExternalTermFormat"]),
+            targets: [
+                "ExternalTermFormat"
+            ]),
     ],
+    traits: ["Embedded"],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ExternalTermFormat",
-            cSettings: shouldBuildForEmbedded
-                ? [
-                    .unsafeFlags(["-fdeclspec"])
-                ]
-                : nil,
-            swiftSettings: shouldBuildForEmbedded
-                ? [
-                    .enableExperimentalFeature("Embedded"),
-                    .unsafeFlags(["-Xfrontend", "-emit-empty-object-file"])
-                ] : nil
+            cSettings: [
+                .unsafeFlags(["-fdeclspec"], .when(traits: ["Embedded"]))
+            ],
+            swiftSettings: [
+                .enableExperimentalFeature("Embedded", .when(traits: ["Embedded"])),
+                .unsafeFlags(["-Xfrontend", "-emit-empty-object-file"], .when(traits: ["Embedded"]))
+            ]
         ),
         .testTarget(
             name: "ExternalTermFormatTests",
